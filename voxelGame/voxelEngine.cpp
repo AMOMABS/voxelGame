994adb147dc19b89450ef6c39df80e703303a835
Arsentiy <82136438+TarkWight@users.noreply.github.com>
2024-02-11 16:44:22 +0700

    Window
    Created window.hpp and window.cpp

---
+++ voxelGame/voxelEngine.cpp
@@ -0,0 +1,45 @@
+﻿#include "glew.h"
+#include "glfw3.h"
+#include <iostream>
+
+//#define GLEW_STATIC
+//#include <GL/glew.h>
+//#include <GL/glfw3.h>
+
+int WIDTH = 1280;
+int HEIGHT = 720;
+
+
+int main(void) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Window", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cerr << "Failed to cteate GLFW Window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glfwInit() != GLEW_OK) {
+        std::cerr << "Failed to initiable GLEW" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
